d968e22a30cf1beec907edecb686d7576babc5ab
lei <mia.wang90@gmail.com>
2022-07-12 18:41:49 +0900

    add search scope

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,2 +1,7 @@
 class Machine < ApplicationRecord
+  scope :ip_search, -> (q) { where("text(ip) ILIKE '%#{q}%' ") }
+
+  def self.ransackable_scopes(_auth_object = nil)
+    [:ip_search]
+  end
 end
